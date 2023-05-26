0a56fa82a6ab61b2dbd636f2ed65caf9860e2d6c
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-05-26 20:47:23 -0300

    feat: add list of site pages and logo risu in footer

--- src/components/Footer/Footer.ts
+++ src/components/Footer/Footer.ts
@@ -6,14 +6,57 @@ const StyledFooter = styled.footer`
   height: 50vh;
   background-color: #111212;
   color: white;
-  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & .container-risu {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  & .link {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  & .risu {
+    height: 60%;
+    width: 60%;
+  }
+
+  & h2 {
+    padding: 2% 0 1% 0;
+    font-size: 2rem;
+  }
+
+  & a {
+    text-decoration: none;
+    color: white;
+    font-size: 1.5rem;
+  }
+
   & .container {
     height: 100%;
     width: 100%;
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    align-items: center;
+    grid-template-columns: repeat(3, 1fr);
+    align-items: start;
     justify-items: center;
+  }
+
+  & .routes ul {
+    list-style-type: none;
+    text-decoration: none;
+  }
+
+  & .routes ul li{
+    margin: 20% 0;
   }
 `;
 
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,26 @@
 import StyledFooter from './Footer';
+import { Link } from 'react-router-dom';
+import risu from '../../assets/logo/risu.png';
 
 function Footer() {
   return (
     <StyledFooter>
+      <h2>Mais informações</h2>
       <div className='container'>
-        <div>aa</div>
-        <div>aa</div>
-        <div>aa</div>
+        <div className='container-risu'>
+          <Link to={{ pathname: 'https://risu.com.br'}} target='_blank' className='link'>
+            <img src={risu} alt="logo-risu" className='risu' />
+          </Link>
+        </div>
+        <nav className='routes'>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/'>Adoção</Link></li>
+            <li><Link to='/'>Notícias</Link></li>
+            <li><Link to='/'>Sobre</Link></li>
+            <li><Link to='/'>Contato</Link></li>
+          </ul>
+        </nav>
         <div>aa</div>
       </div>
     </StyledFooter>
